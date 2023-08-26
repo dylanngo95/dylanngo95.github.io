45b3c3026fb54a8458d35af59931d4bf4497da4d
Dylan Ngo <dylanngo>
2023-08-26 08:53:43 +0800

    update profile image

--- components/sections/index/about.jsx
+++ components/sections/index/about.jsx
@@ -20,12 +20,12 @@ import about from '../../../styles/sections/index/about.module.scss';
  * Section: About
  * An overview of yourself.
  * Highlight your top level attributes and disciplines.
- * 
+ *
  * @returns {jsx} <About />
  */
 export default function About() {
 	return (
-		<Section classProp={about.section}>	
+		<Section classProp={about.section}>
 			<Container spacing={['verticalXXXLrg']}>
 				<SectionTitle
 					title="About Me"
@@ -34,30 +34,30 @@ export default function About() {
 				/>
 				<section className={about.content}>
 					<div className={about.image}>
-						<img src="/img/family-photo.jpg" alt="Nelson family photo"/>
+						<img src="/img/dylanngo.jpg" alt="Nelson family photo"/>
 						{/* <Image src="/img/family-photo.jpg" width={600} height={800}/> */}
 					</div>
 					<div className={about.copy} >
-						<CopyBlock 
+						<CopyBlock
 							title="Softskills that pay the bills"
 							containerClass={about.container}
 							iconClass={about.icon}
 							icon={[ 'fat', 'ear-listen' ]}
 							copy="In addition to my design and technical expertise—I also have strong leadership, time management, and multitasking skills—honed through my experience as a business owner / managing partner, husband, and father of two. Outside of work, I enjoy staying active through sports such as hockey and snowboarding. I am confident in my ability to bring passion and value to any project."
 						/>
-						<BadgesBlock 
-							title="Reasearch and planning" 
+						<BadgesBlock
+							title="Reasearch and planning"
 							containerClass={about.container}
-							list={methods} 
+							list={methods}
 							fullContainer="fullContainer"
-							block="methods" 
+							block="methods"
 							icon="fingerprint"
 							copy="One of my favorite aspects of creating is planning the architecture of a project. From Design Systems to Brand Strategy—I enjoy working with the many touch points of user experience."
 							//invertedColor="invertedColor"
 							headerIcon={`${about.icon}`}
 						/>
 					</div>
-				</section>	
+				</section>
 			</Container>
 		</Section>
 	)
